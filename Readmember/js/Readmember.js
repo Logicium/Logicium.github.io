eb439cf1cb5c2d6911f373ed61310a4ff63aab05
Kisora Thomas <kisora.thomas@gmail.com>
2016-01-10 19:06:18 +0000

    JS

--- Readmember/js/Readmember.js
+++ Readmember/js/Readmember.js
@@ -56,7 +56,7 @@ function assignUserSessions(){
 function setUserInfo(){
     console.log("Setting user info");
     $('#userName').text(user["fields"]["Name"]);
-    $('#userImage').attr('src',  user["fields"]["Profile Image"]);
+    $('#userImage').attr('src', user["fields"]["Profile Image"]);
 }
 
 function setUserStats(){
